d6621ff9020fa1d7082838dfdd581e4f42e30884
exoutia <jhav365@gmail.com>
2024-01-22 14:42:57 +0530

    Adding more functionalities into the todo section
    2024-01-22
    
    today I am going to complete the whole process of adding and deleting the todos and whole crud operations.
    
    1. [x] adding new todo
    2. [x] deleting the todo
    3. [x] marking the todos
    4. [x] updating the todo
    5. [x] getting the more information on the todos

--- app/todo.py
+++ app/todo.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, render_template, request, session, url_for
+from flask import Blueprint, flash, redirect, render_template, request, session, url_for # type: ignore
 
 from .auth import login_required
 from .db import get_db
@@ -10,7 +10,6 @@ bp = Blueprint("todo", __name__, url_prefix="/todo")
 @login_required
 def index():
     user_id = session["user_id"]
-    print(user_id)
     db = get_db()
     todos = db.execute("select * from todo where author_id = ?", (user_id,)).fetchall()
     print([dict(todo) for todo in todos])
@@ -31,9 +30,64 @@ def add():
     return redirect(url_for("todo.index"))
 
 
-@bp.route("/<int:id>", methods=["GET", "POST"])
+@bp.route("/delete/<int:id>", methods=["POST"])
 def delete(id):
     db = get_db()
     db.execute("DELETE FROM todo WHERE id = ?", (id,))
     db.commit()
     return redirect(url_for("todo.index"))
+
+
+@bp.route("/mark/<int:id>", methods=["GET", "POST"])
+def mark(id):
+    db = get_db()
+    status = db.execute("SELECT mark FROM todo WHERE id = ?", (id,)).fetchone()
+    if status is None:
+        flash("Todo not found")
+        return redirect(url_for("todo.index"))
+    status = not status["mark"]
+
+    db.execute(
+        "UPDATE todo SET mark = ? where id = ?",
+        (
+            status,
+            id,
+        ),
+    )
+    db.commit()
+    return redirect(url_for("todo.index"))
+
+
+@bp.route('/<int:id>')
+def todo_page(id):
+    db = get_db()
+    todo = db.execute("SELECT * FROM todo WHERE id = ?", (id,)).fetchone()
+    if todo is None:
+        flash("No todo found with this index.")
+        return redirect(url_for('todo.index'))
+    print(todo)
+    return render_template('todo/todo_page.html', todo=dict(todo))
+
+
+@bp.route("/edit/<int:id>", methods=["POST"])
+def edit(id):
+    db = get_db()
+    todo = db.execute("SELECT * FROM todo WHERE id = ?", (id,)).fetchone()
+    if todo is None:
+        flash("Todo not found")
+        return redirect(url_for("todo.index"))
+
+    if request.method == "POST":
+        title = request.form["title"]
+        body = request.form["body"]
+        db.execute(
+            "UPDATE todo SET title = ?, body = ? where id = ?",
+            (
+                title,
+                body,
+                id,
+            ),
+        )
+        db.commit()
+        flash("done editing")
+    return redirect(url_for("todo.todo_page", id=id))
--- dev_notes.md
+++ dev_notes.md
@@ -13,3 +13,13 @@ Create the login flow and index page for the user.
 1. If the user is logged in the user can directly go to the user index page with three section blog section, todo and meditation section.
 2. If the user is not logged and trying to visit some site specifically meant for the user only then he/she should be redirected to the login page with the option to go the the registration page.
 3. The user should have the option to logout also.
+
+## 2024-01-22
+
+today I am going to complete the whole process of adding and deleting the todos and whole crud operations.
+
+1. [x] adding new todo 
+2. [x] deleting the todo
+3. [x] marking the todos
+4. [x] updating the todo
+5. [x] getting the more information on the todos
